ea5473519a371ceb2abcce034aaf2e766e117e8b
AlanPratama <109650474+AlanPratama@users.noreply.github.com>
2023-12-20 03:42:28 +0700

    create model to represent table

---
+++ app/Models/TaskInd.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TaskInd extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tasks_individu';
+}
